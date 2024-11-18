7595098943b62371bf1e1844b10d5d7a6228c7bb
raven <dastan@zholdas.icu>
2024-11-18 19:49:58 +0500

    Add front, refractoring

--- resources/views/cabinet.blade.php
+++ resources/views/cabinet.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>{{ config('app.name') }}</title>
-    @vite(['resources/js/app.js', 'resources/css/app.css'])
+    @vite(['resources/js/app.js'])
     <script src="https://widget.tiptoppay.kz/bundles/widget.js"></script>
 </head>
 <body>
