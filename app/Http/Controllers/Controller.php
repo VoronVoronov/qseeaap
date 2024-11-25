5f518e57323267236237c7491164eadd1169086c
raven <dastan@zholdas.icu>
2024-11-25 14:30:43 +0500

    Add front, refractoring

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -4,20 +4,20 @@ namespace App\Http\Controllers;
 
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
-use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Http\JsonResponse;
-use Illuminate\Http\Response;
+use Illuminate\Routing\Controller as BaseController;
+use Symfony\Component\HttpFoundation\Response as ResponseAlias;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
 
-    public function sendResponse(array $data, int $customStatus = Response::HTTP_OK)
+    public function sendResponse(array $data, int $customStatus = ResponseAlias::HTTP_OK): JsonResponse
     {
         return response()->json($data, $customStatus);
     }
 
-    public function sendErrorResponse(array $data, int $code = Response::HTTP_BAD_REQUEST)
+    public function sendErrorResponse(array $data, int $code = ResponseAlias::HTTP_BAD_REQUEST): JsonResponse
     {
         return response()->json($data, $code);
     }
